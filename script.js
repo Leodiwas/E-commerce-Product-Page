09769d4887588e62bc700dcb81688b1628510b35
Leo <leodiwas@icloud.com>
2022-03-07 23:41:31 +0545

    appended and modified js codes

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
+"use strict";
+
 const open = document.getElementById("fullImgBox");
 const close = document.getElementById("fullImgBox");
 const fullImg = document.getElementById("fullImg");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const priceEl = document.querySelector(".main-price");
+const discountEl = document.querySelector(".price-discount");
 
-function openfullImg(img) {
+const openfullImg = (img) => {
   open.style.display = "flex";
   fullImg.src = img;
-}
+};
 
-function closeImg() {
+const closeImg = () => {
   close.style.display = "none";
-}
+};
 
 //Quantity Number
-const qNumber = (number) => {
+const quanNumber = (number) => {
   Number((document.getElementById("quan-number").textContent = number));
 };
-const plusEl = document.getElementById("plus");
-const minusEl = document.getElementById("minus");
 
 let value = 0;
+let price = 0;
 
-plusEl.addEventListener("click", () => {
-  if (value < 10) return qNumber(++value);
+const priceFormat = (n) =>
+  Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(n);
+
+const disCount = (n) => {
+  if (n > 500) {
+    discountEl.textContent = `25%`;
+  }
+  if (n > 750) {
+    discountEl.textContent = `50%`;
+  }
+  if (n < 500) {
+    discountEl.textContent = `20%`;
+  }
+  if (n === 0) {
+    discountEl.textContent = `0%`;
+  }
+};
+
+plusBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (value < 5) {
+    quanNumber(++value);
+    price += 125 * 2;
+  }
+  priceEl.textContent = priceFormat(price);
+  disCount(price);
 });
 
-minusEl.addEventListener("click", () => {
-  if (value >= 0) return qNumber(value--);
+minusBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (value >= 0 && price >= 125) {
+    quanNumber(--value);
+    price -= 125 * 2;
+  }
+  priceEl.textContent = priceFormat(price);
+  disCount(price);
 });
