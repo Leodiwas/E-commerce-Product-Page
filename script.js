f211f34cfc95833efefd0e3964f06a386db3538b
Leo <leodiwas@icloud.com>
2022-03-07 08:33:33 +0545

    modified js codes

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const open = document.getElementById("fullImgBox");
 const close = document.getElementById("fullImgBox");
-const fullImg = document.getElementById("fullImg")
+const fullImg = document.getElementById("fullImg");
 
-function openfullImg(img){
-    open.style.display = "flex" ;
-    fullImg.src = img ;
+function openfullImg(img) {
+  open.style.display = "flex";
+  fullImg.src = img;
 }
 
-function closeImg(){
-    close.style.display = "none" ;
+function closeImg() {
+  close.style.display = "none";
 }
+
+//Quantity Number
+const qNumber = (number) => {
+  Number((document.getElementById("quan-number").textContent = number));
+};
+const plusEl = document.getElementById("plus");
+const minusEl = document.getElementById("minus");
+
+let value = 0;
+
+plusEl.addEventListener("click", () => {
+  if (value < 10) return qNumber(++value);
+});
+
+minusEl.addEventListener("click", () => {
+  if (value >= 0) return qNumber(value--);
+});
